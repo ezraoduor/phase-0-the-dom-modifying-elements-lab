27911c8cf35cf63158d4ca8cd906c6b244e05914
Ezra Oduor <ezrachurchill20@gmail.com>
2025-03-02 18:43:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const main = document.querySelector("main#main");
+if (main) {
+    main.remove();
+}
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+
+
+document.body.appendChild(newHeader);
